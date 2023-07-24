bd7f34b44f55d0b44123892449942bd2066e1ac8
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-07-23 22:12:23 -0400

    Add files via upload

---
+++ src/Exogenesis/graphics/ExoPal.java
@@ -0,0 +1,14 @@
+package Exogenesis.graphics;
+
+import arc.graphics.*;
+import mindustry.graphics.*;
+public class ExoPal {
+public static Color
+ genesisLight = Color.valueOf("e1f2ff"),
+ genesis = Color.valueOf("a9d8ff"),
+ genesisDark = Color.valueOf("66b1ff"),
+    empyreanLight = Color.valueOf("fcff98"),
+    empyrean = Color.valueOf("fee761"),
+    empyreanDark = Color.valueOf("f5c045"),
+    empyreanOutline = Color.valueOf("1e1e1e");
+}
