c4da758217dcd438221c3957ea6b2fbc4263f1bc
SiriusA2 <68311340+AureusStratus@users.noreply.github.com>
2023-07-25 23:51:37 -0400

    Add files via upload

--- src/Exogenesis/content/ExoBlocks.java
+++ src/Exogenesis/content/ExoBlocks.java
@@ -35,11 +35,11 @@ public static void load(){
  genesisFactory = new UnitFactory("genesis-factory"){{
   requirements(Category.units, with(Items.copper, 50, Items.lead, 120, Items.silicon, 80));
   plans = Seq.with(
+          new UnitPlan(ExoUnitTypes.lux, 60f * 15, with(Items.silicon, 10, Items.lead, 10)),
           new UnitPlan(ExoUnitTypes.orion, 60f * 15, with(Items.silicon, 10, Items.lead, 10))
   );
   size = 3;
   consumePower(1.2f);
  }};
-
 }
 }
